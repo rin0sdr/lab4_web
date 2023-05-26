30a5226faed1fd1a3498487079b4d186a79fb0c3
rin0sdr <125930612+rin0sdr@users.noreply.github.com>
2023-05-26 14:29:34 +0200

    server.py

--- lab4 web/server.py
+++ lab4 web/server.py
@@ -3,7 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-# Головна сторінка з клієнтською частиною
 @app.route('/')
 def index():
     with open('index.html', 'r') as file:
@@ -15,25 +14,18 @@ def script():
     with open('script.js', 'r') as file:
         return file.read()
 
-# Відповідь на запит GET /styles.css
-#@app.route('/styles.css')
-#def style():
-    with open('styles.css', 'r') as file:
-        return file.read()
+
 
 # Обробка запиту GET /api
 @app.route('/api', methods=['GET'])
 def api():
     # Отримання параметрів запиту
     expression = request.args.get('expression')
-
-    # Виконання обробки або взаємодія з зовнішнім сервісом
-    # та формування відповіді
+    # Виконання обробки
     url = 'https://newton.vercel.app/api/v2/derive/' + expression
     response = requests.get(url)
     data = response.json()
     result = data['result']
-
     return result
 
 if __name__ == '__main__':
